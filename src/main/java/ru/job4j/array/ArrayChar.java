63bb31044e4f0e5c043489c1029c724b601fc5d9
Admin <20nastya93@mail.ru>
2022-09-23 13:57:07 +0700

    Массив заполнен true или false [#53857]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for(int ind = 0; ind < pref.length; ind++) {
+            if (word[ind] != pref[ind]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
