3360211aaa142b0a968b5b5f2d9a48725736651f
Admin <20nastya93@mail.ru>
2022-09-23 14:04:52 +0700

    Слово начинается с ... [#41585 #345901]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        for(int ind = 0; ind < pref.length; ind++) {
+        for (int ind = 0; ind < pref.length; ind++) {
             if (word[ind] != pref[ind]) {
                 result = false;
                 break;
