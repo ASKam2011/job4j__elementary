86b45cf0cb8f165903e58736123929b421b861a6
Admin <20nastya93@mail.ru>
2022-10-01 10:59:26 +0700

    Сортировка выборкой. [#156320]

---
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SortSelectedTest {
+
+    @Test
+    public void whenSort() {
+        int[] data = new int[] {3, 4, 1, 2, 5};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 4, 5};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenSort1() {
+        int[] data = new int[] {3, 2, 5};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {2, 3, 5};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenSort2() {
+        int[] data = new int[] {3, 2, 5, 8, 4, 1, 6};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 4, 5, 6, 8};
+        assertThat(result).containsExactly(expected);
+    }
+}
